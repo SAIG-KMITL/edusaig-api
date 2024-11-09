b2b3eacb5b4b7ed2efce622f5603faac1aaede98
khris-xp <khrisbharmmano@gmail.com>
2024-11-09 11:51:14 +0700

    fix: jwt config typo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { dotenvConfig } from './shared/configs/dotenv.config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
-import { databaseConfig } from './shared/configs/database.config';
-import { GLOBAL_CONFIG } from './shared/constants/global-config.constant';
-import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
+import { databaseConfig } from './shared/configs/database.config';
+import { dotenvConfig } from './shared/configs/dotenv.config';
+import { GLOBAL_CONFIG } from './shared/constants/global-config.constant';
 import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 const forFeatures = TypeOrmModule.forFeature([User]);
 
 @Module({
-    imports: [
-        AuthModule,
-        ConfigModule.forRoot({
-            isGlobal: true,
-            validationSchema: dotenvConfig,
-        }),
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => ({
-                ...databaseConfig,
-                migrations: ["dist/database/migrations/*.js"],
-                migrationsRun: true,
-                synchronize: configService.get<boolean>(GLOBAL_CONFIG.IS_DEVELOPMENT),
-            }),
-            inject: [ConfigService],
-        }),
-        JwtModule.register({
-            global: true,
-        }),
-        DatabaseModule,
-        UserModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: dotenvConfig,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...databaseConfig,
+        migrations: ['dist/database/migrations/*.js'],
+        migrationsRun: true,
+        synchronize: configService.get<boolean>(GLOBAL_CONFIG.IS_DEVELOPMENT),
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.register({
+      global: true,
+    }),
+    DatabaseModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 import { createDatabase } from 'typeorm-extension';
+import { AppModule } from './app.module';
 import { databaseConfig } from './shared/configs/database.config';
 import { GLOBAL_CONFIG } from './shared/constants/global-config.constant';
 
 async function bootstrap() {
-    const configService = new ConfigService()
+  const configService = new ConfigService();
 
-    await createDatabase({
-        options: databaseConfig,
-    });
+  await createDatabase({
+    options: databaseConfig,
+  });
 
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: configService.get<string>(GLOBAL_CONFIG.CORS_ALLOW_ORIGIN),
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    optionsSuccessStatus: 200,
+    exposedHeaders: 'Authorization',
+  });
 
-    app.useGlobalPipes(new ValidationPipe());
-    app.enableCors({
-        origin: configService.get<string>(GLOBAL_CONFIG.CORS_ALLOW_ORIGIN),
-        methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-        optionsSuccessStatus: 200,
-        exposedHeaders: "Authorization",
-    });
-
-    await app.listen(configService.get<string>(GLOBAL_CONFIG.PORT) ?? 3000);
+  await app.listen(configService.get<string>(GLOBAL_CONFIG.PORT) ?? 3000);
 }
 
 bootstrap();
