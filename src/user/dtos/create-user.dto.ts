47e4b7ae0a3bbd6f0960100f8fcbe77aac2c6d97
khris-xp <khrisbharmmano@gmail.com>
2024-11-13 16:17:44 +0700

    feat: pagination query data function

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,42 +1,45 @@
-import { IsString, IsEmail, IsStrongPassword, IsEnum, IsNotEmpty } from "class-validator";
-import { AvailableRoles, Role } from "src/shared/enums/roles.enum";
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty()
-    @ApiProperty({
-        description: 'User email',
-        type: String,
-        example: 'johndoe@gmail.com',
-    })
-    email: string;
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'User email',
+    type: String,
+    example: 'johndoe@gmail.com',
+  })
+  email: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        description: 'User username',
-        type: String,
-        example: 'johndoe',
-    })
-    username: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'User username',
+    type: String,
+    example: 'johndoe',
+  })
+  username: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        description: 'User fullname',
-        type: String,
-        example: 'John Doe',
-    })
-    fullname: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'User fullname',
+    type: String,
+    example: 'John Doe',
+  })
+  fullname: string;
 
-    @IsStrongPassword()
-    @IsNotEmpty()
-    @ApiProperty({
-        description: 'User password',
-        type: String,
-        example: 'P@ssw0rd!',
-    })
-    password: string;
-
+  @IsStrongPassword()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'User password',
+    type: String,
+    example: 'P@ssw0rd!',
+  })
+  password: string;
 }
